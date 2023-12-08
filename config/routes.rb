3625427568637cf31a13afecf43f84fc10805087
knwokike <K.Nwokike@hvd-bb.de>
2023-12-08 14:18:55 +0100

    blog controller update

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
-Rails.application.routes.draw do
-  
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+require 'action_dispatch/routing/mapper'
 
-  get 'blog_posts/:id', to: 'blog_posts#show', as: 'blog_post'
-  get 'blog_posts/new', to: 'blog_posts#new', as: 'new_blog_post'
-  post 'blog_posts', to: 'blog_posts#create', as: 'create_blog_post'
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Example:
+  resources :blog_posts
 
   # Defines the root path route ("/")
   root "blog_posts#index"
