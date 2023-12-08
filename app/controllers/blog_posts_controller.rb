1359778621fdb0d6c659dbfab654af24537572ba
knwokike <K.Nwokike@hvd-bb.de>
2023-12-08 15:27:10 +0100

    added devise for authentification

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -2,8 +2,15 @@ require 'blog_post' # Add the missing import statement
 
 class BlogPostsController < ApplicationController
   before_action :set_blog_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog_post, except: [:index, :new, :create]
+
+  
   def index
+    if user_signed_in?
     @blog_posts = BlogPost.all
+    else
+      redirect_to new_user_session_path
+    end
   end
 
   def show
@@ -13,34 +20,57 @@ class BlogPostsController < ApplicationController
   end
 
   def new
+    if user_signed_in?
     @blog_post = BlogPost.new
+    else
+      redirect_to new_user_session_path
+    end
   end
 
   def create
+    if user_signed_in?
     @blog_post = BlogPost.new(blog_post_params)
+
     if @blog_post.save
       redirect_to @blog_post
     else
       render :new, status: :unprocessable_entity
     end
+
+    else
+      redirect_to new_user_session_path
+    end
   end
 
   def edit
+    if user_signed_in?
     @blog_post = BlogPost.find(params[:id])
+    else
+      redirect_to new_user_session_path
+    end
   end
 
   def update 
+    if user_signed_in?
     if @blog_post.update(blog_post_params)
       redirect_to @blog_post
     else
       render :edit, status: :unprocessable_entity
     end
+
+    else
+      redirect_to new_user_session_path
+    end
   end
 
   def destroy
+    if
     @blog_post = BlogPost.find(params[:id])
     @blog_post.destroy
     redirect_to root_path
+    else
+      redirect_to new_user_session_path
+    end
   end
 
   private
@@ -52,5 +82,13 @@ class BlogPostsController < ApplicationController
     @blog_post = BlogPost.find(params[:id])
   rescue ActiveRecord::RecordNotFound
     redirect_to root_path
+  end
+
+  def authenticate_user!
+    if user_signed_in?
+      super
+    else
+      redirect_to new_user_session_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 require 'action_dispatch/routing/mapper'
 
 Rails.application.routes.draw do
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+
+ # devise_for :users
   # Example:
   resources :blog_posts
 
